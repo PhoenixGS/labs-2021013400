45648911d07b99f79396928bb4e38041585e9c18
PhoenixGS <thestarrydream@gmail.com>
2024-04-08 05:33:34 -0700

    modify sys_get_time
    cherry-pick sys_get_time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,11 +1,12 @@
 use crate::{
     config::MAX_SYSCALL_NUM,
     fs::{open_file, OpenFlags},
-    mm::{translated_ref, translated_refmut, translated_str},
+    mm::{translated_ref, translated_refmut, translated_str, translated_byte_buffer,},
     task::{
         current_process, current_task, current_user_token, exit_current_and_run_next, pid2process,
         suspend_current_and_run_next, SignalFlags, TaskStatus,
     },
+    timer::get_time_us
 };
 use alloc::{string::String, sync::Arc, vec::Vec};
 
@@ -163,11 +164,25 @@ pub fn sys_kill(pid: usize, signal: u32) -> isize {
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
-        current_task().unwrap().process.upgrade().unwrap().getpid()
-    );
-    -1
+    trace!("kernel: sys_get_time");
+    let us = get_time_us();
+
+    let buffers = translated_byte_buffer(current_user_token(), _ts as *mut u8, core::mem::size_of::<TimeVal>());
+
+    let ts  = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+
+    let mut ptr = &ts as *const TimeVal as *const u8;
+    for buffer in buffers {
+        let len = buffer.len();
+        unsafe {
+            buffer.copy_from_slice(core::slice::from_raw_parts(ptr, len));
+            ptr = ptr.add(len);
+        }
+    }
+    0
 }
 
 /// task_info syscall
