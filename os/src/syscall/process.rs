bce47f9f3a9cd6edc05808cd865c5cab90346385
PhoenixGS <thestarrydream@gmail.com>
2024-05-11 08:13:42 -0700

    update sys_spawn

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -205,11 +205,33 @@ pub fn sys_sbrk(size: i32) -> isize {
 /// HINT: fork + exec =/= spawn
 pub fn sys_spawn(_path: *const u8) -> isize {
     trace!(
-        "kernel:pid[{}] sys_spawn NOT IMPLEMENTED",
+        "kernel:pid[{}] sys_spawn",
         current_task().unwrap().pid.0
     );
-    
-    -1
+
+    // let token = current_user_token();
+    // let path = translated_str(token, _path);
+    // if let Some(data) = get_app_data_by_name(path.as_str()) {
+    //     let current_task = current_task().unwrap();
+    //     let new_task = current_task.spawn(data);
+    //     add_task(new_task);
+    //     new_task.pid.0 as isize
+    // } else {
+    //     -1
+    // }
+
+    let token = current_user_token();
+    let path = translated_str(token, _path);
+    if let Some(data) = get_app_data_by_name(path.as_str()) {
+        let current_task = current_task().unwrap();
+        let new_task = current_task.fork();
+        let new_pid = new_task.pid.0;
+        new_task.exec(data);
+        add_task(new_task);
+        new_pid as isize
+    } else {
+        -1
+    }
 }
 
 // YOUR JOB: Set task priority.
